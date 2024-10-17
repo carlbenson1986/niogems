47094f9f14b414a876a624ea64e669c7ef54f5c8
Carl <rskv0327@gmail.com>
2024-10-17 19:00:05 +0100

    fsdfsd

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.use(bodyParser.json());
 const dbConfig = {
   user: 'SYS',
   password: 'N0raD3mr#1', // Replace with your actual password
-  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=NIOGEMS-RDS)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=NIOGEMS)))',
-  privilege: oracledb.SYSDBA
+  connectString: '20.231.195.79:1521/NIOGEMS',
 };
 
 // Function to check if a table exists
