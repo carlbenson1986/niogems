990a4e8628723e480cd3bf8f17b86275cc1c505b
Carl <rskv0327@gmail.com>
2024-10-17 17:23:50 +0100

    ssdfsdfsdfs

--- server.js
+++ server.js
@@ -7,27 +7,14 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://niogems-front.vercel.app', // Correctly specify the allowed origin
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true // If your request requires credentials (cookies, HTTP authentication)
-}));
-
+app.use(cors());
 app.use(bodyParser.json());
 
-// Handle preflight requests
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://niogems-front.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.sendStatus(200);
-});
 // Database connection details
 const dbConfig = {
   user: 'SYS',
   password: 'N0raD3mr#1', // Replace with your actual password
-  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=20.231.195.79))(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=NIOGEMS)))',
+  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=NIOGEMS-RDS)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=NIOGEMS)))',
   privilege: oracledb.SYSDBA
 };
 
@@ -48,13 +35,10 @@ async function checkColumnExists(connection, tableName, columnName) {
   );
   return result.rows[0][0] > 0;
 }
-app.get('/', (req, res) => {
-  res.send("Welcome to the API");
-});
+
 // User login endpoint
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  console.log('sfsd', username, password);
   let connection;
 
   try {
@@ -82,7 +66,7 @@ app.post('/login', async (req, res) => {
       // Retrieve OFFICE_GROUP_ID from NIO_USERS
       const officeResult = await connection.execute(
         `SELECT OFFICE_GROUP_ID FROM NIOGEMS_USERs.NIO_USERS WHERE LOGIN_ID = :loginId`,
-        { loginId: 'GGAMBRELL' }
+        { loginId: username }
       );
 
       const officeGroupId = officeResult.rows.length > 0 ? officeResult.rows[0][0] : null;
