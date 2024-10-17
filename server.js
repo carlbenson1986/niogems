e3d5ec4711bd47e779ffd8ea88c8223adf2ca6b4
Carl <rskv0327@gmail.com>
2024-10-17 18:55:19 +0100

    sfsdfsdfsd

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const dbConfig = {
   user: 'SYS',
   password: 'N0raD3mr#1', // Replace with your actual password
-  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=NIOGEMS-RDS)(PORT=1521))(CONNECT_DATA=(SID=CLRExtProc)))',
+  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=NIOGEMS-RDS)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=NIOGEMS)))',
   privilege: oracledb.SYSDBA
 };
 
